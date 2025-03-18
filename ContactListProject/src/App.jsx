b63eced0c8face918eda49043b19e850e24c3730
lleifhei <loganleifheit@gmail.com>
2025-03-18 17:18:24 -0400

    rendered fake contacts

--- ContactListProject/src/App.jsx
+++ ContactListProject/src/App.jsx
@@ -2,34 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import ContactList from './components/ContactList'
+import dummyContacts from './components/ContactList'
+const api = fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users")
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [contacts, setContacts] = useState(dummyContacts)
+  console.log(contacts)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ContactList />
     </>
   )
 }
 
-export default App
+export default App;
